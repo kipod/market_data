798312e6783c333707ab1b11c990b024edb37d4a
Nikolay <nchernov@algotradinghub.com>
2019-10-22 16:53:06 +0300

    time format

--- market_data/const.py
+++ market_data/const.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timezone
 
-
 DATA_SIZE = 8 + 8 + 4 + (4 * 4)
 
 MAX_LATENCY_TIME = 20 * 1000 * 1000
@@ -8,8 +7,12 @@ MAX_LATENCY_TIME = 20 * 1000 * 1000
 ONE_SEC_IN_NANOSECOND = 1000 * 1000 * 1000
 
 
+def time_from_timestamp(timestamp: int) -> datetime:
+    return datetime.fromtimestamp(timestamp // ONE_SEC_IN_NANOSECOND, timezone.utc)
+
+
 def str_time_from_timestamp(timestamp: int) -> str:
-    dt = datetime.fromtimestamp(timestamp // ONE_SEC_IN_NANOSECOND, timezone.utc)
+    dt = time_from_timestamp(timestamp)
     s = dt.strftime('%H:%M:%S')
     s += '.' + str(int(timestamp % ONE_SEC_IN_NANOSECOND)).zfill(9)
     return s
---
+++ statistic/statistic.py
@@ -0,0 +1,21 @@
+from market_data.const import str_time_from_timestamp
+
+class Statistic(object):
+    def __init__(self, file_name: str = 'out.csv'):
+        self.file_name = file_name
+        self.__file = open(file_name, 'wt')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        self.__file.close()
+
+    def __del__(self):
+        self.close()
+
+    def __repr__(self):
+        return "file: {}".format(self.file_name)
