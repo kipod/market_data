f051840591b809200fc5b73a65eb95b381b91d37
nikolay <nchernov@algotradinghub.com>
2019-10-28 03:18:37 -0400

    unix run update

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # 2019_10_14
-# parse.py --market=/mnt/data/md/2019_10_14/market_ARCA-30.csv.bin --CQS=/mnt/data/md/2019_10_14/CQS_ARCA_P-30.csv.bin --out=/mnt/data/md/2019_10_14/ARCA_CQS.csv
-# parse.py --market=/mnt/data/md/2019_10_14/market_ARCA-30.csv.bin --CQS=/mnt/data/md/2019_10_14/CQS_ARCA_P-30.csv.bin --out=/mnt/data/md/2019_10_14/ARCA_CQS_1.csv --interval=1
-# parse.py --market=/mnt/data/md/2019_10_14/market_ARCA-30.csv.bin --CTS=/mnt/data/md/2019_10_14/CTS_ARCA-30.csv.bin   --out=/mnt/data/md/2019_10_14/ARCA_CTS.csv
-# parse.py --market=/mnt/data/md/2019_10_14/market_ARCA-30.csv.bin --CTS=/mnt/data/md/2019_10_14/CTS_ARCA-30.csv.bin   --out=/mnt/data/md/2019_10_14/ARCA_CTS_1.csv --interval=1
+# ./parse.py --market=/mnt/data/md/2019_10_14/market_ARCA-30.csv.bin --CQS=/mnt/data/md/2019_10_14/CQS_ARCA_P-30.csv.bin --out=/mnt/data/md/2019_10_14/ARCA_CQS.csv
+# ./parse.py --market=/mnt/data/md/2019_10_14/market_ARCA-30.csv.bin --CTS=/mnt/data/md/2019_10_14/CTS_ARCA-30.csv.bin   --out=/mnt/data/md/2019_10_14/ARCA_CTS.csv
 # ./parse.py --market=/mnt/data/md/2019_10_14/market_NASDAQ-30.csv.bin --UQDF=/mnt/data/md/2019_10_14/UQDF_NASDAQ_Q-30.csv.bin --out=/mnt/data/md/2019_10_14/NASDAQ_UQDF.csv
-./parse.py --market=/mnt/data/md/2019_10_14/market_NASDAQ-30.csv.bin --UTDF=/mnt/data/md/2019_10_14/UTDF_NASDAQ_Q-30.csv.bin --out=/mnt/data/md/2019_10_14/NASDAQ_UTDF.csv
+# ./parse.py --market=/mnt/data/md/2019_10_14/market_NASDAQ-30.csv.bin --UTDF=/mnt/data/md/2019_10_14/UTDF_NASDAQ_Q-30.csv.bin --out=/mnt/data/md/2019_10_14/NASDAQ_UTDF.csv
+# ./parse.py --market=/mnt/data/md/2019_10_14/market_BATS-30.csv.bin --CTS=/mnt/data/md/2019_10_14/CTS_BATS-30.csv.bin --out=/mnt/data/md/2019_10_14/BATS_CTS.csv
+# ./parse.py --market=/mnt/data/md/2019_10_14/market_BATS-30.csv.bin --CQS=/mnt/data/md/2019_10_14/CQS_BATS_Z-30.csv.bin --out=/mnt/data/md/2019_10_14/BATS_CQS.csv
+./parse.py --market=/mnt/data/md/2019_10_14/market_NYSE-30.csv.bin --CTS=/mnt/data/md/2019_10_14/CTS_NYSE-30.csv.bin --out=/mnt/data/md/2019_10_14/NYSE_CTS.csv
+./parse.py --market=/mnt/data/md/2019_10_14/market_NYSE_10222-30.csv.bin --CQS=/mnt/data/md/2019_10_14/CQS_NYSE_N-30.csv.bin --out=/mnt/data/md/2019_10_14/NYSE_CQS.csv
 
 # 2019_10_15
 # python3 parse.py --market=/mnt/data/md/2019_10_15/market_ARCA-30.csv.bin --CQS=/mnt/data/md/2019_10_15/CQS_ARCA_P-30.csv.bin --out=/mnt/data/md/2019_10_15/CQS.csv
